24d416064e1250dcc66bc21d9bf35ebce3384ba2
pitsu94 <pitsu94@gmail.com>
2026-01-12 10:18:25 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   
   return {
-    // השורה הבאה היא החשובה ביותר עבור GitHub Pages
-    // היא אומרת ל-Vite שהאתר נמצא תחת התיקייה NM.LandPage
-    base: '/NM.LandPage/',
+    // השורה שמתקנת את הגישה לקבצים ב-GitHub Pages
+    base: '/NM.LandPage/', 
     
     server: {
       port: 3000,
